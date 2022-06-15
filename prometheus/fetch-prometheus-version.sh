0b1ba2d78a08a7114c1c96995324e9082c1135da
Arfan <arfanpantua@gmail.com>
2022-06-15 15:30:04 +0700

    Prometheus

---
+++ prometheus/fetch-prometheus-version.sh
@@ -0,0 +1,2 @@
+helm list | grep prometheus | awk '{print "PROM_CHART_VERSION="$9}' >> prometheus.version.bak
+helm list | grep prometheus | awk '{print "PROM_APP_VERSION="$10}' >> prometheus.version.bak
