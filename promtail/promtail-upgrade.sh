0b3ae2f3ecb3ab3af4dfc3b555bddbef284f72fa
Arfan Pantua <105478366+arfanpantua88@users.noreply.github.com>
2022-07-27 11:17:00 +0700

    Chores promtail wrapper script (#9)
    * set backoff_config full
    
    * update service Client
    
    * Promtail Version echo
    
    * resource limit
    
    Co-authored-by: Arfan <arfanpantua@gmail.com>

--- promtail/promtail-upgrade.sh
+++ promtail/promtail-upgrade.sh
@@ -24,14 +24,25 @@ helm get values promtail | tee $PROMTAIL_VALUES
 cp $PROMTAIL_VALUES "$PROMTAIL_VALUES.bak"
 cat << EOF >> $PROMTAIL_VALUES
 loki:
-  serviceName: "loki.$LOKI_NAMESPACE"
+  serviceName: loki.$LOKI_NAMESPACE
 config:
   client:
     # Maximum wait period before sending batch
     batchwait: 5s
+    backoff_config:
+      # Initial backoff time between retries
+      min_period: 100ms
+      # Maximum backoff time between retries
+      max_period: 5m
+      # Maximum number of retries when sending batches, 0 means infinite retries
+      max_retries: 20
+resources:
+ limits:
+   cpu: 0.2
+   memory: 800Mi
 EOF
 
-echo "-- Upgrade the helm: $LOKI_VERSION"
+echo "-- Upgrade the helm: $PROMTAIL_VERSION"
 kubectl label pods -l app=promtail,release=$PROMTAIL_RELEASE_NAME app.kubernetes.io/name=promtail app.kubernetes.io/instance=$PROMTAIL_RELEASE_NAME
 kubectl delete daemonset -l app=promtail,release=$PROMTAIL_RELEASE_NAME --cascade=false
 helm repo add promtail https://grafana.github.io/helm-charts
