063a53fba06edbdcc236db8e8d1f09771aec1a8a
arfanpantua88 <arfan@hydrax.io>
2023-12-24 10:36:17 +0700

    Initiate

--- grafana/fetch-grafana-current-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -x
-export GF_NAMESPACE=...
-kubectl config set-context --current --namespace=$GF_NAMESPACE
-helm list | grep grafana | awk '{print "GF_CHART_VERSION="$9}' >> grafana.version.bak
-helm list | grep grafana | awk '{print "GF_APP_VERSION="$10}' >> grafana.version.bak
--- loki/fetch-loki-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -x
-export NAMESPACE=...
-kubectl config set-context --current --namespace=$NAMESPACE
-helm list | grep loki | awk '{print "LOKI_CHART_VERSION="$9}' >> loki.version.bak
-helm list | grep loki | awk '{print "LOKI_APP_VERSION="$10}' >> loki.version.bak
